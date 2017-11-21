989ec7cfac4f3f9f421e3a46f24c1deebd0dd689
HashimotoYT <hashimoto.stream@gmail.com>
2017-11-21 19:29:55 +0200

    Add styles

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from './app'
 
+if (!process.env.TEST_ENV) {
+  require('../../styles/hellstorm.scss')
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
