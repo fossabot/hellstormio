2037a1f189fa1db786f2096d9f4c31a4b02065c0
HashimotoYT <hashimoto.stream@gmail.com>
2017-11-22 21:13:35 +0200

    Add nav bar component

---
+++ src/ui/nav-bar/nav-bar.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export class NavBar extends React.Component {
+  public render() {
+    return (
+      <div id='navbar' className='navbar'>
+        <div><a href='#'>Test1</a></div>
+        <div><a href='#'>Test2</a></div>
+        <div><a href='#'>Test2</a></div>
+      </div>
+    )
+  }
+}
