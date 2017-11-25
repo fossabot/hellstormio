ac4526b7ae3c5c2e57c221ec94c6d2d4b00ea188
HashimotoYT <hashimoto.stream@gmail.com>
2017-11-25 22:00:03 +0200

    Start using React Router in Nav Bar component

--- src/ui/nav-bar/nav-bar.tsx
+++ src/ui/nav-bar/nav-bar.tsx
@@ -1,13 +1,42 @@
 import * as React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 export class NavBar extends React.Component {
   public render() {
     return (
-      <ul id='navbar' className='navbar'>
-        <li><a href='#'>Test1</a></li>
-        <li><a href='#'>Test2</a></li>
-        <li><a href='#'>Test2</a></li>
-      </ul>
+      <Router>
+        <div id='navbar'>
+          <ul className='navbar'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path='/' component={this.homeComponent}/>
+          <Route exact path='/' component={this.aboutComponent}/>
+          <Route exact path='/' component={this.topicsComponent}/>
+        </div>
+      </Router>
     )
   }
+
+  public homeComponent = () => (
+    <div>
+      Home
+    </div>
+  )
+
+  public aboutComponent = () => (
+    <div>
+      About Page
+    </div>
+  )
+
+  public topicsComponent = () => (
+    <div>
+      Topics
+    </div>
+  )
 }
