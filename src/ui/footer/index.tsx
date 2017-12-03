2e94edd06ec433f922683b1aa78f596f40715ecd
HashimotoYT <hashimoto.stream@gmail.com>
2017-12-03 22:08:32 +0200

    Add footer class component

---
+++ src/ui/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export class Footer extends React.Component {
+  public render() {
+    return (
+      <div className='footer'>
+        <p>Footer</p>
+      </div>
+    )
+  }
+}
