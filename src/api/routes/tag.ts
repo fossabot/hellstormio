42ddb80a663282e2e7d16e7dc129580578f1be54
HashimotoYT <hashimoto.stream@gmail.com>
2017-12-07 23:36:16 +0200

    This is works fine

--- src/api/routes/tag.ts
+++ src/api/routes/tag.ts
@@ -1,7 +1,9 @@
 import * as express from 'express'
-const router = express.Router()
 import { standardOutput, connection } from '../database'
 
+const router = express.Router()
+
+/* GET tag listing. */
 router.get('/', function (req, res) {
   connection.query('SELECT * FROM `tag`', function (error, results) {
     standardOutput(error, results, res)
@@ -18,23 +20,13 @@ router.post('/', function (req, res) {
   })
 })
 
+/* PUT tag listing. */
 router.put('/', function (req, res) {
-  connection.query('UPDATE `tag` SET `name`=?, where `id`=?', [req.body.id], function (error, results, fields) {
-    if (error) {
-      res.json({
-        'status': 500,
-        'error': error,
-        'response': null,
-      })
-      // If there is error, we send the error in the error section with 500 status
-    } else {
-      res.json({
-        'status': 200,
-        'error': null,
-        'response': results,
-      })
-      // If there is no error, all is good and response is 200OK.
-    }
+  let query = 'UPDATE `tag` SET ?? = ? WHERE `tag`.`id` = ?'
+  const table = [`name`, req.body.name, req.body.id]
+  query = connection.format(query, table)
+  connection.query(query, function (error, results) {
+    standardOutput(error, results, res)
   })
 })
 
