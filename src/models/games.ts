b353afabcdad8185ca6d92df3e50aa7a3d6227fd
HashimotoYT <hashimoto.stream@gmail.com>
2017-12-07 19:19:47 +0200

    Add Games and User Models

---
+++ src/models/games.ts
@@ -0,0 +1,6 @@
+export interface IGames {
+  readonly id: string
+  readonly name: string
+  readonly description: string
+  readonly release_date: string
+}
---
+++ src/models/user.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  readonly id: string
+  readonly username: string
+  readonly email: string
+  readonly password: string
+  readonly join_date: string
+}
