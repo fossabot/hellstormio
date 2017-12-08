f5cdd699a5cf227411034d5af0584fbcc82bab61
HashimotoYT <hashimoto.stream@gmail.com>
2017-12-08 15:21:07 +0200

    Add senty

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,9 @@
 import * as express from 'express'
+const Raven = require('raven')
 // import { connection } from './database/connect'
 const app = express()
+
+Raven.config('https://a2465449ea624f42ac28ade5df68ffe0:620853d383054857977debe629e96101@sentry.io/224389').install()
 
 //#region Import routes
 const users = require('./routes/users')
@@ -35,6 +38,7 @@ app.use(function (req, res, next) {
 
 // Enable server
 export function connect() {
+  Raven.requestHandler(),
   app.listen(9222, 'localhost', (err: any) => {
     if (err) {
       console.log(err)
@@ -44,4 +48,5 @@ export function connect() {
 
     console.log(`Server running at http://localhost:9222`)
   })
+  Raven.errorHandler()
 }
